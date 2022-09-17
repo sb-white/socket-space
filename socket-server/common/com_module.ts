d9f3842374ee0e4f6e7669986bac40de52443d63
Martin Stöcker <martin.stoecker@pexon-consulting.de>
2022-09-17 17:09:15 +0200

    common folder

---
+++ socket-server/common/com_module.ts
@@ -0,0 +1,26 @@
+interface ServerToClientEvents {
+    noArg: () => void;
+    basicEmit: (a: number, b: string, c: Buffer) => void;
+    withAck: (d: string, callback: (e: number) => void) => void;
+}
+  
+interface ClientToServerEvents {
+    hello: () => void;
+}
+
+interface InterServerEvents {
+    ping: () => void;
+}
+
+interface SocketData {
+    name: string;
+    age: number;
+}
+
+export interface PlayerPosition{
+    x: number; 
+    y: number; 
+    angle: number; // 0 to 360
+
+}
+
